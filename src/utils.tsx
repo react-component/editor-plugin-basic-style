ac8b51eaa4e4386172c54346863f30b1ca24eb58
RaoHai <surgesoft@gmail.com>
2017-02-07 15:54:23 +0800

    {} -> null

--- src/utils.tsx
+++ src/utils.tsx
@@ -117,7 +117,7 @@ export function getToggleEntityFunc(callbacks) {
         editorState.getCurrentContent(),
         selection,
         ' ',
-        {},
+        null,
         active ? null : entityKey,
       );
     } else {
